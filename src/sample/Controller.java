277c5cb89f9d342be087f4e188b174316fc2fd1a
a.forghan <a.forghani.y79@gmail.com>
2019-11-25 22:42:54 +0330

    NumPad matched to numbers and operators buttons Back Space and Delete keys matched to Del and CE buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,18 +5,17 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
-import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    private StringBuilder text=new StringBuilder("");
     private boolean isOperatorPressed=false;
     private String operator="";
     private double temp=0.0 , sum=0.0;
-
 
     @FXML
     private JFXTextField textField;
@@ -37,6 +36,99 @@ public class Controller implements Initializable {
             }
             isOperatorPressed=false;
         }
+    }
+
+    private boolean isDotPressed = false;
+
+    @FXML
+    public void keyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().isKeypadKey()) {
+            onPressNumberKey(keyEvent);
+        } else if (keyEvent.getCode().toString().equals("BACK_SPACE")) {
+            onClickDel();
+        } else if (keyEvent.getCode().toString().equals("DELETE")) {
+            onClickCE();
+        } else {
+            onPressDotKey(keyEvent);
+            onPressOperatorKey(keyEvent);
+        }
+        // System.out.println("keyPressed: "+ keyEvent.getCode().toString());
+    }
+
+    public void onPressDotKey(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.DECIMAL) {
+            if (!isDotPressed) {
+                if (isOperatorPressed) {
+                    textField.setText("0.");
+
+                } else {
+                    textField.setText(textField.getText().trim() + ".");
+                }
+                isOperatorPressed = false;
+            }
+            isDotPressed = true;
+        }
+
+    }
+
+
+    public void onPressNumberKey(KeyEvent keyEvent) {
+        if (keyEvent.getCode().isKeypadKey()) {
+            if (isOperatorPressed) {
+                textField.setText(keyEvent.getText().trim());
+            } else {
+                textField.setText(textField.getText() + keyEvent.getText().trim());
+            }
+            isOperatorPressed = false;
+        }
+    }
+
+    public void onPressOperatorKey(KeyEvent keyEvent) {
+        if (!textField.getText().isEmpty()) {
+            temp = Double.parseDouble(textField.getText());
+            switch (operator) {
+                case "/":
+                    sum /= temp;
+                    break;
+                case "X":
+                    sum *= temp;
+                    break;
+                case "+":
+                    sum += temp;
+                    break;
+                case "-":
+                    sum -= temp;
+                    break;
+                default:
+                    sum = temp;
+            }
+        }
+
+        if (keyEvent.getCode().toString().equals("ENTER")) {
+            textField.setText(String.valueOf(sum));
+            operator = "";
+        } else {
+            textField.setText("");
+            isDotPressed = false;
+            operator = keyEvent.getText().trim();
+        }
+        isOperatorPressed = true;
+    }
+
+    @FXML
+    public void onClickDot() {
+        if (!isDotPressed) {
+            if (isOperatorPressed) {
+                textField.setText("0.");
+
+            } else {
+                textField.setText(textField.getText().trim() + ".");
+            }
+            isOperatorPressed = false;
+        }
+        isDotPressed = true;
+
+
     }
 
     @FXML
@@ -67,30 +159,28 @@ public class Controller implements Initializable {
                 operator="";
             }else {
                 textField.setText("");
+                isDotPressed = false;
                 operator=btn.getText().trim();
             }
             isOperatorPressed=true;
         }
     }
 
-
-
     @FXML
-    public void onClickDel(ActionEvent event){
+    public void onClickDel() {
         if (textField.getText().length()>0){
             textField.setText(textField.getText(0,textField.getText().length()-1));
         }
     }
 
     @FXML
-    public void onClickCE(ActionEvent event){
+    public void onClickCE() {
         textField.setText("");
         temp=0.0;
         sum=0.0;
         isOperatorPressed=false;
         operator="";
   }
-
 
     }
 
